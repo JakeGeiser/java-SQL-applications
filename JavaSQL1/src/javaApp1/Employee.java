52c27fc54c71314d00ce4b7fd957fc05d491326b
jgeis <jgeis@VENGEANCE>
2021-04-21 09:18:17 -0400

    added property variables

--- JavaSQL1/src/javaApp1/Employee.java
+++ JavaSQL1/src/javaApp1/Employee.java
@@ -2,4 +2,11 @@ package javaApp1;
 
 public class Employee {
 	// https://www.tutorialspoint.com/java/java_encapsulation.htm
+	 private int id;
+	 private String name;
+	 private String dept;
+	 private String role;
+	 private int salary;
+	 
+	 
 }
