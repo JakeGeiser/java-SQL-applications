a047ac3eda66ce4b6ea18be51cffa088ee7005f2
jgeis <jgeis@VENGEANCE>
2021-04-21 10:19:43 -0400

    filled in featureFilter method

--- JavaSQL1/src/javaApp1/DbManager.java
+++ JavaSQL1/src/javaApp1/DbManager.java
@@ -27,7 +27,21 @@ public class DbManager {
 	}
 	
 	public ArrayList<Employee> featureFilter(String field, String title){
-			
+		ArrayList<ArrayList<Object>> queryResult = query.filter(field, title);
+		ArrayList<Employee> managerResult = new ArrayList<Employee>();
+		
+		for (ArrayList<Object> row : queryResult) {
+			// declare a temporary employee object to be added to list
+			Employee tempName = new Employee();
+			tempName.setID(Integer.parseInt(row.get(0).toString()));
+			tempName.setName(row.get(1).toString());
+			tempName.setAge(Integer.parseInt(row.get(2).toString()));
+			tempName.setDepartment(row.get(3).toString());
+			tempName.setRole(row.get(4).toString());
+			tempName.setSalary(Integer.parseInt(row.get(5).toString()));
+			managerResult.add(tempName);
+		}
+		return managerResult;
 	}
 
 	public ArrayList<Employee> featureReport(){
