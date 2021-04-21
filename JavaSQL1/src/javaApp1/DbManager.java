0ca1aca180c904fa230742daa9cc0a41381d098d
jgeis <jgeis@VENGEANCE>
2021-04-21 10:18:43 -0400

    completed featureSearch method

--- JavaSQL1/src/javaApp1/DbManager.java
+++ JavaSQL1/src/javaApp1/DbManager.java
@@ -6,14 +6,29 @@ public class DbManager {
 	// determines what query to execute
 	// call QueryExecute
 	// build result from results of QueryExecute
+	QueryExe query = new QueryExe();
 	
 	public ArrayList<Employee> featureSearch(String name){
+		ArrayList<ArrayList<Object>> queryResult = query.search(name);
+		ArrayList<Employee> managerResult = new ArrayList<Employee>();
 		
+		for (ArrayList<Object> row : queryResult) {
+			// declare a temporary employee object to be added to list
+			Employee tempName = new Employee();
+			tempName.setID(Integer.parseInt(row.get(0).toString()));
+			tempName.setName(row.get(1).toString());
+			tempName.setAge(Integer.parseInt(row.get(2).toString()));
+			tempName.setDepartment(row.get(3).toString());
+			tempName.setRole(row.get(4).toString());
+			tempName.setSalary(Integer.parseInt(row.get(5).toString()));
+			managerResult.add(tempName);
+		}
+		return managerResult;
 	}
 	
-	public ArrayList<Employee> featureFilter(String name){
+	public ArrayList<Employee> featureFilter(String field, String title){
 			
-		}
+	}
 
 	public ArrayList<Employee> featureReport(){
 		
