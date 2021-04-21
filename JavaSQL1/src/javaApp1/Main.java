73a62fe94a7a0664ea8161f16ccc303b693c4e6e
jgeis <jgeis@VENGEANCE>
2021-04-21 13:00:19 -0400

    added pre-while loop stdout

--- JavaSQL1/src/javaApp1/Main.java
+++ JavaSQL1/src/javaApp1/Main.java
@@ -1,11 +1,32 @@
 package javaApp1;
+import java.util.*;
 
 public class Main {
 	// interact to get input 
 	// call manager class
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
-
+		String proceed0 = "yes";
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println("Features");
+		System.out.println("=========");
+		System.out.println("1. Search");
+		System.out.println("2. Filter");
+		System.out.println("3. Report");
+		System.out.println("=========");
+		
+		while(proceed0=="yes") {
+			
+		}
+		
+		
+		
+		
+		
+		
+		scan.close();
 	}
 
 }
