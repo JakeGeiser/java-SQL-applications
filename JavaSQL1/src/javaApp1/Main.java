c31eb2aa3ff4ad2a404087955730ea0ad0e4da53
jgeis <jgeis@VENGEANCE>
2021-04-20 22:23:19 -0400

    removed filler from main

--- JavaSQL1/src/javaApp1/Main.java
+++ JavaSQL1/src/javaApp1/Main.java
@@ -6,35 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 
-//		try {
-//			
-//			ResultSet rs = stmt.executeQuery("SELECT name, age FROM employee");
-//			while(rs.next()) {
-//				String name = rs.getString(1);
-//				int age = rs.getInt(2);
-//				System.out.println(name + " | "+age);
-//			}
-//			
-//			
-//			// deal with results
-//			
-//			
-//			
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//				con.close();
-//			} catch (SQLException e) {
-//				// ignored
-//			}
-//		}
-//		
-//		// query 
-//		
-//		// deal with results
-
 	}
 
 }
