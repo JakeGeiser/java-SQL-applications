a08cee751b4a0c2e840884686f99f9975f152b3d
jgeis <jgeis@VENGEANCE>
2021-04-21 09:21:51 -0400

    added methods to getVariable

--- JavaSQL1/src/javaApp1/Employee.java
+++ JavaSQL1/src/javaApp1/Employee.java
@@ -4,9 +4,28 @@ public class Employee {
 	// https://www.tutorialspoint.com/java/java_encapsulation.htm
 	 private int id;
 	 private String name;
-	 private String dept;
+	 private String deptartment;
 	 private String role;
 	 private int salary;
 	 
+	 public int getID() {
+		 return id;
+	 }
+	 
+	 public String getName() {
+		 return name;
+	 }
+	 
+	 public String getDepartment() {
+		 return deptartment;
+	 }
+	 
+	 public String getRole() {
+		 return role;
+	 }
+	 
+	 public int getSalary() {
+		 return salary;
+	 }
 	 
 }
